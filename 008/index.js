dd57d95c91e3f01714185aadf13665cf848f721b
Demon <Demon62git>
2024-10-29 20:30:12 +0300

    ref_убрал закомментированный кусочек

--- 008/index.js
+++ 008/index.js
@@ -10,8 +10,6 @@ app.set("view engine", "ejs");
 
 app.use(logger);
 app.use('/', viewRouter);
-
-// app.use('/api', router);
 app.use(err404);
 app.use(express.json());
 app.listen(port, () => console.log(`Ручки можно проверить на http://localhost:${port}`));
